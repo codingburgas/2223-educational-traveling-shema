b950186bf899483f400a74d3b191a7ef0b1dead6
ESBogdanov20 <85444197+ESBogdanov20@users.noreply.github.com>
2022-10-22 15:50:11 +0300

    perf(backend): make a function that sets all variables

--- app/main.cpp
+++ app/main.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	Menu menu(900, 800, "Game");
     menu.toggleFullScreenWindow(menu.GetDisplayWidth(), menu.GetDisplayHeight());
+    menu.SetAll({ menu.GetDisplayWidth() / 2.0f, -100, 150, 150 }, 0.0f, 1.0f, 0, 0);
 
     while (!menu.GameShouldClose())
     {
