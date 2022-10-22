c4615a6a163cc29554ed249abf93d653f062e8e6
ESBogdanov20 <85444197+ESBogdanov20@users.noreply.github.com>
2022-10-22 23:11:22 +0300

    style(backend): make everything camel case

--- app/main.cpp
+++ app/main.cpp
@@ -4,15 +4,15 @@
 int main()
 {
 	Menu menu(900, 800, "Game");
-    menu.toggleFullScreenWindow(menu.GetDisplayWidth(), menu.GetDisplayHeight());
-    menu.SetAll({ menu.GetDisplayWidth() / 2.0f, -100, 150, 150 }, 0.0f, 1.0f, 0, 0, LoadTexture("resources/Play.png"), false,
+    menu.toggleFullScreenWindow(menu.getDisplayWidth(), menu.getDisplayHeight());
+    menu.setAll({ menu.getDisplayWidth() / 2.0f, -100, 150, 150 }, 0.0f, 1.0f, 0, 0, LoadTexture("resources/Play.png"), false,
         LoadTexture("resources/Options.png"), LoadTexture("resources/Rules.png"), LoadTexture("resources/Exit.png"),
         LoadTexture("resources/nott.png"), LoadTexture("resources/topLeftCorner.png"), LoadTexture("resources/topRightCorner.png"),
         LoadTexture("resources/bottomLeftCorner.png"), LoadTexture("resources/bottomRightCorner.png"));
 
-    while (!menu.GameShouldClose())
+    while (!menu.gameShouldClose())
     {
-        menu.Animation();
+        menu.animation();
 
         BeginDrawing();
         ClearBackground(BLACK);
@@ -23,6 +23,6 @@ int main()
         EndDrawing();
     }
 
-    menu.UnloadTextures();
+    menu.unloadTextures();
     return 0;
 }
