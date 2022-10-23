2bf2297ead187b76953ef8d08df255b33f83db21
Maxim Marinov <MMMarinov20@codingburgas.bg>
2022-10-23 21:03:51 +0300

    fix(backend): fix file structure

---
+++ app/include/Menu.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <string>
+#include <raylib.h>
+class Menu
+{
+public:
+    Menu(int width, int height, std::string title);
+    ~Menu() noexcept;
+
+    bool gameShouldClose() const;
+
+    void toggleFullScreenWindow(int windowWidth, int windowHeight);
+
+    int getDisplayWidth() const;
+    int getDisplayHeight() const;
+
+    void animation();
+    void drawAnimation();
+    void checkAnimation();
+
+    void mainMenu();
+
+    void setAll(Rectangle rect, float rot, float alp, int sta, float frmsCounter, Texture2D pl, bool anim_end,
+        Texture2D opt, Texture2D rls, Texture2D exit, Texture2D nott, Texture2D topLeftCorner, Texture2D topRightCorner,
+        Texture2D bottomLeftCorner, Texture2D bottomRightCorner);
+
+    void unloadTextures();
+
+private:
+    Texture2D nameOfTheTeam;
+    Texture2D play;
+    Texture2D options;
+    Texture2D rules;
+    Texture2D quit;
+
+    Texture2D topLCorner;
+    Texture2D topRCorner;
+    Texture2D bottomLCorner;
+    Texture2D bottomRCorner;
+
+    Rectangle rec;
+    float rotation = 0.0f;
+    float alpha = 0.0f;
+
+    bool animationEnd = false;
+
+    int state = 0;
+    float framesCounter = 0;
+};
