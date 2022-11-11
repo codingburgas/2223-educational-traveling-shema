8056cfaae67f495f370a6fb135826a2256860904
Maxim Marinov <MMMarinov20@codingburgas.bg>
2022-11-11 16:27:20 +0200

    refactor(backend): resolve PR comments

--- app/src/Menu.cpp
+++ app/src/Menu.cpp
@@ -1,13 +1,9 @@
 #include "Menu.hpp"
 Menu::Menu()
 {
-    gameManager->LoadScene(gameManager->SCENE::MAIN_MENU,
-                           {"TopLeftCorner.png", "TopRightCorner.png", "BottomLeftCorner.png", "BottomRightCorner.png", "Logo.png"},
-                           {{0, 0}, {this->width - 600, 0}, {0, this->height - 500}, {this->width - 600, this->height - 500}, {this->width / 2 - 750 / 2, this->height / 2 - 200 / 2 - 250}});
+    gameManager->LoadScene(gameManager->SCENE::MAIN_MENU, this->clouds, this->cloudsPositions);
 
-    gameManager->LoadButtons({"Play.png", "Options.png", "Rules.png", "Exit.png"},
-                             {"PlayHover.png", "OptionsHover.png", "RulesHover.png", "ExitHover.png"},
-                             {{this->width / 2 - 250 / 2 - 20, this->height / 2 - 75 / 2 - 10}, {this->width / 2 - 250 / 2 - 20, this->height / 2 - 75 / 2 + 85}, {this->width / 2 - 250 / 2 - 20, this->height / 2 - 75 / 2 + 180}, {this->width / 2 - 250 / 2 - 20, this->height / 2 - 75 / 2 + 275}});
+    gameManager->LoadButtons(this->buttons, this->buttonsHover, this->buttonPositions);
 
     while (gameManager->MAIN_MENU && !gameManager->getShouldClose())
     {
@@ -45,7 +41,7 @@ void Menu::animation()
     {
         this->framesCounter += 2.00f;
 
-        this->rect.y = EaseElasticOut((float)this->framesCounter, -100, height / 2.0f + 100, 120);
+        this->rect.y = EaseElasticOut((float)this->framesCounter, - 100, this->height / 2.0f + 100, 120);
 
         if (this->framesCounter >= 120)
         {
@@ -57,7 +53,7 @@ void Menu::animation()
     {
         this->framesCounter += 2.00f;
         this->rect.height = EaseBounceOut((float)this->framesCounter, 100, -90, 120);
-        this->rect.width = EaseBounceOut((float)this->framesCounter, 100, width + 180, 120);
+        this->rect.width = EaseBounceOut((float)this->framesCounter, 100, this->width + 180, 120);
 
         if (this->framesCounter >= 120)
         {
@@ -79,7 +75,7 @@ void Menu::animation()
     case 3:
     {
         this->framesCounter += 3.00f;
-        this->rect.height = EaseCircOut((float)this->framesCounter, 10, width + 180, 120);
+        this->rect.height = EaseCircOut((float)this->framesCounter, 10, this->width + 180, 120);
 
         if (this->framesCounter >= 80)
             this->animationEnd = true;
