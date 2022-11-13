88afa8c9ff21002ec042663707e7cca620c6e90e
Maxim Marinov <MMMarinov20@codingburgas.bg>
2022-11-13 20:36:21 +0200

    refactor(backend): optimize waypoint rendering

--- app/include/MapManager.hpp
+++ app/include/MapManager.hpp
@@ -9,7 +9,11 @@ public:
 
 private:
 	GameManager *gameManager = GameManager::getInstance();
+	Vector2 m_mousePos = { 0, 0 };
+	Texture2D waypoint = LoadTexture("./assets/Waypoint.png");
 	const int mapWidth = 1920;
 	const int mapHeight = 1080;
-	void DrawWaypoints(Texture2D waypoint);
+	std::vector<Vector2> waypoints = { {350,50} , {450,220} , {605,605} , {480,560} , {800,430} , {940,350} , {830,650} , {1200,830} , {700,750} , {500,900} , {350,915} , {750,625} , {730,660} , {1100,340} , {1200,750} , {1170,930} , {870,830} , {1300,660} , {1200,550} , {1030,600} , {1400,880} , {950,670} , {950,730} , {800,750} , {955,775} , {1000,785} , {1100,810} , {1150,860} , {1110,900} , {1040,830} , {1090,855} , {1050,760} , {1050,700} , {1130,520} , {1140,480} , {1135,435} , {1300,450} , {830,540} , {1290,710} };
+	std::vector<float> scale = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.75, 0.75, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.75, 0.75, 1, 1, 0.75, 1, 0.5, 1, 0.75, 1, 0.75, 0.75, 1, 0.75, 0.75 };
+	void DrawWaypoints(std::vector<Vector2> waypoints, std::vector<float> scale);
 };
--- app/src/MapManager.cpp
+++ app/src/MapManager.cpp
@@ -3,13 +3,13 @@
 MapManager::MapManager()
 {
 	gameManager->LoadScene(gameManager->SCENE::GAME, {"Map.png"}, {{gameManager->screenSize().x / 2 - this->mapWidth / 2, gameManager->screenSize().y / 2 - this->mapHeight / 2}});
-	Texture2D waypoint = LoadTexture("./assets/Waypoint.png");
 	while (gameManager->currentScene == gameManager->SCENE::GAME && !gameManager->getShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(BLUE);
+		this->m_mousePos = GetMousePosition();
 		gameManager->Update();
-		DrawWaypoints(waypoint);
+		DrawWaypoints(this->waypoints, this->scale);
 		EndDrawing();
 	}
 }
@@ -19,51 +19,14 @@ MapManager::~MapManager()
 	
 }
 
-void MapManager::DrawWaypoints(Texture2D waypoint)
+void MapManager::DrawWaypoints(std::vector<Vector2> waypoints, std::vector<float> scale)
 {
-	ClearBackground(BLUE);
-	DrawTexture(waypoint, 350, 50, WHITE); // Greenland
-	DrawTexture(waypoint, 450, 220, WHITE); // Iceland
-	DrawTexture(waypoint, 605, 605, WHITE); // England
-	DrawTexture(waypoint, 480, 560, WHITE); // Ireland
-	DrawTexture(waypoint, 800, 430, WHITE); // Norway
-	DrawTexture(waypoint, 940, 350, WHITE); // Sweden
-	DrawTexture(waypoint, 830, 650, WHITE); // Germany
-	DrawTexture(waypoint, 1200, 830, WHITE); // Bulgaria
-	DrawTexture(waypoint, 700, 750, WHITE); // France
-	DrawTexture(waypoint, 500, 900, WHITE); // Spain
-	DrawTexture(waypoint, 350, 915, WHITE); // Portugal 
-	DrawTextureEx(waypoint, { 750, 625 }, 0, 0.75, WHITE); // Netherlands
-	DrawTextureEx(waypoint, { 730, 660 }, 0, 0.75, WHITE); // Belgium
-	DrawTexture(waypoint, 1100, 340, WHITE); // Finland
-	DrawTexture(waypoint, 1200, 750, WHITE); // Romania
-	DrawTexture(waypoint, 1170, 930, WHITE); // Greece
-	DrawTexture(waypoint, 870, 830, WHITE); // Italy
-	DrawTexture(waypoint, 1300, 660, WHITE); // Ukraine
-	DrawTexture(waypoint, 1200, 550, WHITE); // Belarus
-	DrawTexture(waypoint, 1030, 600, WHITE); // Poland
-	DrawTexture(waypoint, 1400, 880, WHITE); // Turkey
-	DrawTexture(waypoint, 950, 670, WHITE); // Czech R
-	DrawTexture(waypoint, 950, 730, WHITE); // Austria
-	DrawTexture(waypoint, 800, 750, WHITE); // Switzerland
-	DrawTextureEx(waypoint, { 955, 775 }, 0, 0.75, WHITE); // Slovenia
-	DrawTextureEx(waypoint, { 1000, 785 }, 0, 0.75, WHITE); // Croatia
-	DrawTexture(waypoint, 1100, 810, WHITE); // Serbia
-	DrawTexture(waypoint, 1150, 860, WHITE); // Macedonia
-	DrawTextureEx(waypoint, { 1110, 900 }, 0, 0.75, WHITE); // Albania
-	DrawTexture(waypoint, 1040, 830, WHITE); // Bosnia
-	DrawTextureEx(waypoint, { 1090, 855 }, 0, 0.5, WHITE); // Montenegro
-	DrawTexture(waypoint, 1050, 760, WHITE); // Hungary
-	DrawTextureEx(waypoint, { 1050, 700 }, 0, 0.75, WHITE); // Slovakia
-	DrawTexture(waypoint, 1130, 520, WHITE); // Lithuania
-	DrawTextureEx(waypoint, { 1140, 480 }, 0, 0.75, WHITE); // Latvia
-	DrawTextureEx(waypoint, { 1135, 435 }, 0, 0.75, WHITE); // Estonia
-	DrawTexture(waypoint, 1300, 450, WHITE); // Russia
-	DrawTextureEx(waypoint, { 830, 540 }, 0, 0.75, WHITE); // Denmark
-	DrawTextureEx(waypoint, { 1290, 710 }, 0, 0.75, WHITE); // Moldova
-
-
-
-
-
+	for (size_t i = 0; i < waypoints.size(); i++)
+	{
+		DrawTextureEx(this->waypoint, waypoints[i],0 , scale[i], WHITE);
+		if (CheckCollisionPointRec(this->m_mousePos, { waypoints[i].x, waypoints[i].y, this->waypoint.width * scale[i], this->waypoint.height * scale[i] }))
+		{
+			SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+		}
+	}
 }
