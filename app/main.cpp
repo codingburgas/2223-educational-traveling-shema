5707520e75a3eadf361ed1675806c7adc6942a6b
HHChandar20 <HHChandar20@codingburgas.bg>
2022-11-17 23:00:09 +0200

    feat(backend): create game window

--- app/main.cpp
+++ app/main.cpp
@@ -19,6 +19,18 @@ int main()
 
 int main()
 {
+<<<<<<< HEAD
 
 >>>>>>> 3b6da91 (feat(backend): add necessary libraries for the sea game)
+=======
+	InitWindow(1920, 1080, "Mini Game Waves");
+	
+	while (!WindowShouldClose())
+    	{
+        		BeginDrawing();
+        		HideCursor();
+        		ClearBackground(RAYWHITE);
+        	
+	}
+>>>>>>> be9e7d5 (feat(backend): create game window)
 }
