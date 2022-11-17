3c715e19b69e8f8c959264cd3a94eddce0238c2b
SHHristov20 <SHHristov20@codingburgas.bg>
2022-11-17 23:00:26 +0200

    fix(frontend): fix button misalignment

--- app/src/Game.cpp
+++ app/src/Game.cpp
@@ -60,22 +60,35 @@ void Game::ChooseStartingCountry()
 			DrawText("Are you sure you\n want to choose", gameManager->getScreenSize().x / 2 - MeasureText("Are you sure you want to choose", 60) / 4 - 20, gameManager->getScreenSize().y / 2 - 180, 60, BLACK);
 			DrawText(clickedCountry.c_str(), gameManager->getScreenSize().x / 2 - MeasureText(clickedCountry.c_str(), 60) / 2, gameManager->getScreenSize().y / 2, 60, RED);
 			
-			DrawTextureEx(YesButton, { gameManager->getScreenSize().x / 2 - YesButton.width * 3 + 50, gameManager->getScreenSize().y / 2 + YesButton.height }, 0, 2, WHITE);
-			if (CheckCollisionPointRec(GetMousePosition(), {gameManager->getScreenSize().x / 2 - YesButton.width * 3 + 50, gameManager->getScreenSize().y / 2 + YesButton.height, float(YesButton.width * 2), float(YesButton.height * 2)}))
+			
+			SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+			DrawTextureEx(YesButton, { gameManager->getScreenSize().x / 2 - YesButton.width * 3 + 50, gameManager->getScreenSize().y / 2 + YesButton.height + 50 }, 0, 2, WHITE);
+			if (CheckCollisionPointRec(GetMousePosition(), {gameManager->getScreenSize().x / 2 - YesButton.width * 3 + 50, gameManager->getScreenSize().y / 2 + YesButton.height + 50, float(YesButton.width * 2), float(YesButton.height * 2)}))
 			{
-				DrawTextureEx(YesButton, {gameManager->getScreenSize().x / 2 - YesButton.width * 3 + 50, gameManager->getScreenSize().y / 2 + YesButton.height}, 0, 2, Fade(WHITE, 0.5f));
+				SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+				DrawTextureEx(YesButton, {gameManager->getScreenSize().x / 2 - YesButton.width * 3 + 50, gameManager->getScreenSize().y / 2 + YesButton.height + 50}, 0, 2, Fade(BLACK, 0.5f));
 				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
 				{
 					mapManager->setChosenCountry(mapManager->isWaypointClicked().name);
+					UnloadTexture(ModalWindow);
+					UnloadTexture(YesButton);
+					UnloadTexture(NoButton);
 					chosen = true;
 				}
 			}
 			
-			DrawTextureEx(NoButton, { gameManager->getScreenSize().x / 2 + NoButton.width - 50, gameManager->getScreenSize().y / 2 + NoButton.height }, 0, 2, WHITE);
-			if (CheckCollisionPointRec(GetMousePosition(), { gameManager->getScreenSize().x / 2 + NoButton.width - 50, gameManager->getScreenSize().y / 2 + NoButton.height, float(NoButton.width * 2), float(NoButton.height * 2) }))
+			DrawTextureEx(NoButton, { gameManager->getScreenSize().x / 2 + NoButton.width - 50, gameManager->getScreenSize().y / 2 + NoButton.height + 50 }, 0, 2, WHITE);
+			if (CheckCollisionPointRec(GetMousePosition(), { gameManager->getScreenSize().x / 2 + NoButton.width - 50, gameManager->getScreenSize().y / 2 + NoButton.height + 50, float(NoButton.width * 2), float(NoButton.height * 2) }))
 			{
-				DrawTextureEx(NoButton, { gameManager->getScreenSize().x / 2 + NoButton.width - 50, gameManager->getScreenSize().y / 2 + NoButton.height }, 0, 2, Fade(WHITE, 0.5f));
-				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) break;
+				SetMouseCursor(MOUSE_CURSOR_POINTING_HAND);
+				DrawTextureEx(NoButton, { gameManager->getScreenSize().x / 2 + NoButton.width - 50, gameManager->getScreenSize().y / 2 + NoButton.height + 50 }, 0, 2, Fade(BLACK, 0.5f));
+				if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+				{
+					UnloadTexture(ModalWindow);
+					UnloadTexture(YesButton);
+					UnloadTexture(NoButton);
+					break;
+				}
 			}
 			
 			EndDrawing();
