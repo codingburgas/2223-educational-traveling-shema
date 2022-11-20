8f709eba29abe31804a8f78510d6b64c21e53a80
Maxim Marinov <MMMarinov20@codingburgas.bg>
2022-11-20 21:49:11 +0200

    feat(backend): implement mission randomizer

--- app/include/Game.hpp
+++ app/include/Game.hpp
@@ -3,6 +3,7 @@
 #include <MapManager.hpp>
 #include <SeaMinigame.hpp>
 #include <GetRich.hpp>
+#include <random>
 
 class Game
 {
@@ -10,8 +11,8 @@ public:
 	Game();
 	~Game();
 private:
-	GameManager *gameManager = GameManager::GetInstance();
-	MapManager *mapManager = new MapManager();
+	GameManager* gameManager = GameManager::GetInstance();
+	MapManager* mapManager = new MapManager();
 	void ChooseCountryAnimation(bool displayText);
 	void ChooseStartingCountry();
 	bool chosen = false;
@@ -21,11 +22,14 @@ private:
 	bool ClickedCountryUnlocked;
 	int ClickedCountryPrice;
 	void LoadDynamicTextures();
-	
+	void showMissions();
+	std::string currentCountry = mapManager->getPlayerCountry();
+	std::vector<std::string> missions = { "Clean the sea", "Captcha solver", "Clean the street", "Get rich", "Hangman", "Mail packager" };
+
 	int balance = 500;
 	void PassiveIncome();
 	float time = 0;
-	
+
 	int tickets = 1;
 
 	Texture2D CountryHUDTexture;
@@ -33,18 +37,21 @@ private:
 
 	Texture2D Checkbox;
 	Texture2D Checkmark;
-	
+
 	Texture2D UnlockButton;
 	Texture2D UnlockButtonHover;
 	Texture2D UnlockButtonLocked;
 	Texture2D Unlocked;
-	
+
 	Texture2D TravelButton;
 	Texture2D TravelButtonHover;
 	Texture2D TravelButtonLocked;
-	
+
 	Texture2D TravelTicketButton;
 	Texture2D TravelTicketButtonHover;
 	Texture2D TravelTicketButtonLocked;
-	
+
+	Texture2D MissionContainer;
+	Texture2D MissionContainerHover;
+	Texture2D CompletedMission;
 };
