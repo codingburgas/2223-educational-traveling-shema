ac787d71f62b19f0c92a739b6a3bd40d4df0929d
HHChandar20 <HHChandar20@codingburgas.bg>
2022-11-20 16:07:38 +0200

    feat(backend): initialize game window

--- app/src/MailPackager.cpp
+++ app/src/MailPackager.cpp
@@ -4,5 +4,16 @@
 
 int main()
 {
+    InitWindow(1920, 1080, "Mail Packager");
 
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BROWN);
+        EndDrawing();
+     }
+
+    CloseWindow();
+    return 0;
 }
