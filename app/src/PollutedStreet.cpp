614a02bcb3a191a64d6259874b3121645ac081f6
HHChandar20 <HHChandar20@codingburgas.bg>
2022-11-20 17:40:56 +0200

    feat(backend): Initialize game window

--- app/src/PollutedStreet.cpp
+++ app/src/PollutedStreet.cpp
@@ -2,5 +2,19 @@
 
 int main()
 {
-	
+    InitWindow(1920, 1080, "Polluted Street");
+    
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
