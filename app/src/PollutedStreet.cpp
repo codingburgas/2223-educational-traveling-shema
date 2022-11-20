991d41f73e08179ff683dd847d78759de748937f
HHChandar20 <HHChandar20@codingburgas.bg>
2022-11-20 17:48:24 +0200

    feat(backend): add cleaning bar assets

--- app/src/PollutedStreet.cpp
+++ app/src/PollutedStreet.cpp
@@ -21,6 +21,14 @@ int main()
                             789, 724, 844, 779, 719, GetScreenHeight() / 2 + 270, GetScreenHeight() - bin.height - 20,
                             GetScreenHeight() / 2 + 190 };
 
+    Texture2D bar[12] =
+    {
+        LoadTexture("../assets/PollutedStreet/bar1.png"), LoadTexture("../assets/PollutedStreet/bar2.png"), LoadTexture("../assets/PollutedStreet/bar3.png"),
+        LoadTexture("../assets/PollutedStreet/bar4.png"), LoadTexture("../assets/PollutedStreet/bar5.png"), LoadTexture("../assets/PollutedStreet/bar6.png"),
+        LoadTexture("../assets/PollutedStreet/bar7.png"), LoadTexture("../assets/PollutedStreet/bar8.png"), LoadTexture("../assets/PollutedStreet/bar9.png"),
+        LoadTexture("../assets/PollutedStreet/bar10.png"), LoadTexture("../assets/PollutedStreet/bar11.png"), LoadTexture("../assets/PollutedStreet/bar12.png")
+    };
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
