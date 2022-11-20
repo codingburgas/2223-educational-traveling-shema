d87718295eda52d6eab7cadf9dee0394406df21a
HHChandar20 <HHChandar20@codingburgas.bg>
2022-11-20 16:10:00 +0200

    feat(frontend): add game assets

--- app/src/MailPackager.cpp
+++ app/src/MailPackager.cpp
@@ -6,13 +6,19 @@ int main()
 {
     InitWindow(1920, 1080, "Mail Packager");
 
+    Texture2D packages[4] = { LoadTexture("../assets/mail-packager/package-front.png"), LoadTexture("../assets/mail-packager/package-front-with-stamp.png"),LoadTexture("../assets/mail-packager/package-back.png"), LoadTexture("../assets/mail-packager/package-back-with-stamp.png") };
+    Texture2D letters[4] = { LoadTexture("../assets/mail-packager/letter-front.png"), LoadTexture("../assets/mail-packager/letter-front-with-stamp.png"),LoadTexture("../assets/mail-packager/letter-back.png"), LoadTexture("../assets/mail-packager/letter-back-with-stamp.png") };
+    Texture2D background = LoadTexture("../assets/mail-packager/background.png");
+    Texture2D finishScreen = LoadTexture("../assets/mail-packager/finish-screen.png");
+
     SetTargetFPS(60);
+	
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(BROWN);
         EndDrawing();
-     }
+    }
 
     CloseWindow();
     return 0;
