bdc7e7e79a46a6a3cc5d5dd352427457bbfa69b8
HHChandar20 <HHChandar20@codingburgas.bg>
2022-11-20 12:06:43 +0200

    feat(backend): include neccessary libraries

---
+++ app/src/CaptchaSolver.cpp
@@ -0,0 +1,13 @@
+#include <raylib.h>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    InitWindow(1920, 1080, "Captcha Solver");
+
+    SetTargetFPS(60);
+
+    CloseWindow();
+    return 0;
+}
