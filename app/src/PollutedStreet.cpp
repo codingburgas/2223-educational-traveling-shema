ebe87c0e322b7e6340f9c1ec6431be54ead8e9b3
HHChandar20 <HHChandar20@codingburgas.bg>
2022-11-20 17:43:43 +0200

    feat(backend): add polluted street game assets

--- app/src/PollutedStreet.cpp
+++ app/src/PollutedStreet.cpp
@@ -3,7 +3,17 @@
 int main()
 {
     InitWindow(1920, 1080, "Polluted Street");
+
+    Texture2D city = LoadTexture("../assets/PollutedStreet/city.png");
     
+    Texture2D rubbish = LoadTexture("../assets/PollutedStreet/rubbish.png");
+    Texture2D rubbish2 = LoadTexture("../assets/PollutedStreet/rubbish2.png");
+    Texture2D chips = LoadTexture("../assets/PollutedStreet/chips.png");
+    Texture2D bin = LoadTexture("../assets/PollutedStreet/rubbish-bin.png");
+    Texture2D food = LoadTexture("../assets/PollutedStreet/food.png");
+    Texture2D food2 = LoadTexture("../assets/PollutedStreet/food2.png");
+    Texture2D paper = LoadTexture("../assets/PollutedStreet/paper.png");    
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
